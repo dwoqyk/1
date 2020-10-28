5778e45db766b3f6354a708bb001bdd71367dd71
dwoqyk <73614443+dwoqyk@users.noreply.github.com>
2020-10-29 01:01:12 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,13 @@ termux-setup-storage
 #Asks for storage permissions
 
 sleep 5s
-apt update && apt upgrade && apt install python ffmpeg
+apt update && apt upgrade -y 
+#Updates and upgrades termux pakages
+
+echo "..."
+echo "installing python "
+pkg install python -y 
+#Installs python
 
 echo "..."
 echo "installing youtube-dl"
@@ -11,19 +17,19 @@ pip install youtube-dl
 
 echo "..."
 echo "configuring youtube-dl"
-mkdir /data/data/com.termux/files/home/storage/shared/Download/Youtube-downloads 
+mkdir /data/data/com.termux/files/home/storage/shared/Youtube-downloads 
 #Creates folder where the videos will be downloaded
 
 mkdir -p ~/.config/youtube-dl  
 #Creates youtube-dl config folder
 
-echo '-o /data/data/com.termux/files/home/storage/shared/Download/Youtube-downloads/%(extractor_key)s/%(uploader)s/%(title)s-%(id)s.%(ext)s' >> ~/.config/youtube-dl/config 
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s -f "best[height<=1440]"' >> ~/.config/youtube-dl/config 
 #Creates config file for youtube-dl in "[height<=1080]" 1080 can be replace for any other resolution, this will set the maximum resolution available that will be downloaded
 
 mkdir ~/bin  
 #Creates bin foler for termux-url-opener
 
-echo 'url=$1 youtube-dl $url' >> ~/bin/termux-url-opener 
+echo 'youtube-dl $1' >> ~/bin/termux-url-opener 
 #Creates file to open youtube links by directly sharing with termux app
 
 cd ~
